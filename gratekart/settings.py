895cddf93e83b742b936c0093388d0f7bd73ee59
surbhi <funnygirlsurbhi@gmail.com>
2022-11-12 13:46:34 +0530

    django authentication

--- gratekart/settings.py
+++ gratekart/settings.py
@@ -135,3 +135,18 @@ MEDIA_ROOT=BASE_DIR /'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+
+}
+
+#smtp configuration
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER ='funnygirlsurbhi@gmail.com'
+
+EMAIL_HOST_PASSWORD = 'ukhbxqhgkqpvfqss'
+EMAIL_USE_TLS = True
